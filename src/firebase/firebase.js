1c84ce100603e504f91634a03e1fc24163369d03
HristijanMKD <hristijangorgioski501@gmail.com>
2020-03-30 10:20:32 +0200

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,42 @@
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/firebase-firestore";
+
+const config = {
+  // ENTER HERE YOUR CREDENTIALS FROM FIREBASE !!! !!! !!!
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+    this.db = app.firestore();
+  }
+
+  login(email, password) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+
+  async register(name, email, password) {
+    await this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth.currentUser.updateProfile({
+      displayName: name
+    });
+  }
+
+  isInitialized() {
+    return new Promise(resolve => {
+      this.auth.onAuthStateChanged(resolve);
+    });
+  }
+
+  getCurrentUsername() {
+    return this.auth.currentUser && this.auth.currentUser.displayName;
+  }
+}
+
+export default new Firebase();
